3257de7706752bfef830ba3597182a00f301c3b4
Harsini SR <harsini.ramalingam@gmail.com>
2022-09-13 11:27:33 +0530

    Resizable clock without number allignment

--- AnalogueClock/MainPage.xaml.cs
+++ AnalogueClock/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 namespace AnalogueClock
@@ -19,17 +20,20 @@ namespace AnalogueClock
     {
         DispatcherTimer timer = new DispatcherTimer();
         public string clockState = "twelve";
-     
+        public int TweleveHrDotRadius = 120, TwentyFourHrDotRadius = 135;
+        
+
         TextBlock[] VisibilityTextBlocks;
         TextBlock[] AllTextBlocks;
         List<TextBlock> allDotsTextBlock = new List<TextBlock>();
         public MainPage()
         {
             this.InitializeComponent();
+
             //initialize to modify visiblity and access all textboxes
             TextBlock[] addtextBlocks = { textblock13, textblock14, textblock15, textblock16, textblock17, textblock19, textblock20, textblock21, textblock22, textblock23, textblock3, textblock9 };
-            TextBlock[] addall = {top,left,right,bottom, textblock13, textblock14, textblock15, textblock16, textblock17, textblock19, textblock20, textblock21, textblock22, textblock23, textblock3, textblock9,textblock1,textblock10,textblock11,textblock2,textblock4,textblock5,textblock7,textblock8 };
-
+            TextBlock[] addall = { top, left, right, bottom, textblock13, textblock14, textblock15, textblock16, textblock17, textblock19, textblock20, textblock21, textblock22, textblock23, textblock3, textblock9, textblock1, textblock10, textblock11, textblock2, textblock4, textblock5, textblock7, textblock8 };
+          
             VisibilityTextBlocks = addtextBlocks;
             AllTextBlocks = addall;
 
@@ -47,21 +51,43 @@ namespace AnalogueClock
             timer.Start();
             TwelveHrClock();
         }
+
+        //increase size
+        public void IncreaseClockSize()
+        {
+            NumberCanvas.Width += 20; NumberCanvas.Height += 20;
+            outerBlack.Width += 20; outerBlack.Height += 20;
+            innerBlack.Width += 20; innerBlack.Height += 20;
+            gray.Height += 20; gray.Width += 20;
+            secondHand.X1 += 10;minuteHand.X1 += 10;hourHand.X1 += 7;
+        }
+
+        public void DecreaseClockSize()
+        {
+            NumberCanvas.Width -= 20; NumberCanvas.Height -= 20;
+            outerBlack.Width -= 20; outerBlack.Height -= 20;
+            innerBlack.Width -= 20; innerBlack.Height -= 20;
+            gray.Height -= 20; gray.Width -= 20;
+            secondHand.X1 -=10;minuteHand.X1 -= 10;hourHand.X1 -= 6;
             
+        }
+
         //set for 12 hr format
         public void TwelveHrClock()
         {
             //set allignment
             int[] CanvasAlignmentValues = { 0, 95, 100, 0, 100, 200, 200, 100 };
             SetCanvasProperties(CanvasAlignmentValues);
-            NumberCanvas.Width = 220;NumberCanvas.Height = 235;
-            outerBlack.Width = 280;outerBlack.Height = 280;
+            NumberCanvas.Width = 220; NumberCanvas.Height = 235;
+            outerBlack.Width = 280; outerBlack.Height = 280;
             innerBlack.Width = 270; innerBlack.Height = 270;
             gray.Height = 260; gray.Width = 260;
 
+
+
             //switch style,visibility and text
             Style s = (Style)Resources["twelveHrFont"];
-            foreach(var i in AllTextBlocks)
+            foreach (var i in AllTextBlocks)
             {
                 i.Style = s;
             }
@@ -69,7 +95,7 @@ namespace AnalogueClock
             {
                 i.Visibility = Visibility.Collapsed;
             }
-            
+
             top.Text = "12"; bottom.Text = "6"; right.Text = "3"; left.Text = "9";
 
             //subscribe to 12 hr tick
@@ -78,45 +104,26 @@ namespace AnalogueClock
             //calculate position align numbers r initialangle valuenumberblock buffer
             List<TextBlock> valueNumberBlocks = new List<TextBlock> { textblock1, textblock2, textblock4, textblock5, textblock7, textblock8, textblock10, textblock11 };
             valueNumberBlocks.Reverse();
-            calculateNumberAlignment(98, -240, valueNumberBlocks, 2, 30);
+             calculateNumberAlignment(98, -240, valueNumberBlocks, 2, 30);
 
             //align dots
-            double rdot = 120, inc = 6, count = 0;
+            AlignDots(TweleveHrDotRadius, 6, true);
 
-            foreach (var i in allDotsTextBlock)
-            {
-                double x1dot, y1dot;
-               
-                if (count % 5 == 0)
-                {
-                    i.FontSize = 40;
-                     x1dot = (130 + rdot * Math.Cos(Math.PI * (count * inc) / 180.0)-4);
-                     y1dot = (110 - rdot * Math.Sin(Math.PI * (count * inc) / 180.0)-20);
-                }
-                else
-                {
-                     x1dot = (130 + rdot * Math.Cos(Math.PI * (count * inc) / 180.0));
-                     y1dot = (110 - rdot * Math.Sin(Math.PI * (count * inc) / 180.0));
-                }
-             
-                i.SetValue(Canvas.LeftProperty, x1dot);
-                i.SetValue(Canvas.TopProperty, y1dot);
-                count++;
-            }
         }
 
-     //set for 24 hr format
+        //set for 24 hr format
         public void TwentyFourHrClock()
         {
             //set allignment
             int[] CanvasAlignmentValues = { -20, 100, 100, -20, 100, 220, 220, 100 };
+
             SetCanvasProperties(CanvasAlignmentValues);
-            NumberCanvas.Width = 220;NumberCanvas.Height = 230;
-            outerBlack.Width = 310;outerBlack.Height = 310;
+            NumberCanvas.Width = 220; NumberCanvas.Height = 230;
+            outerBlack.Width = 310; outerBlack.Height = 310;
             innerBlack.Width = 300; innerBlack.Height = 300;
             gray.Height = 290; gray.Width = 290;
 
-        
+
             //switch style,visibility and text
             Style s = (Style)Resources["twentyfourHrFont"];
             foreach (var i in AllTextBlocks)
@@ -137,49 +144,27 @@ namespace AnalogueClock
             // calculate number positions
             List<TextBlock> valueNumberBlocks = new List<TextBlock> { textblock1, textblock2, textblock3, textblock4, textblock5, textblock7, textblock8, textblock9, textblock10, textblock11, textblock13, textblock14, textblock15, textblock16, textblock17, textblock19, textblock20, textblock21, textblock22, textblock23 };
             valueNumberBlocks.Reverse();
-            calculateNumberAlignment(120, -255, valueNumberBlocks, 5, 15);
-           
+              calculateNumberAlignment(120, -255, valueNumberBlocks, 5, 15);
 
             //calculate dot positions
-            double rdot = 135, inc = 15, count = 0;
-            foreach (var i in allDotsTextBlock)
-            {
-                i.FontSize = 20;
-                double x1dot = (130 + rdot * Math.Cos(Math.PI * (count * inc) / 180.0));
-                double y1dot = (110 - rdot * Math.Sin(Math.PI * (count * inc) / 180.0));
-                i.SetValue(Canvas.LeftProperty, x1dot);
-                i.SetValue(Canvas.TopProperty, y1dot);
-                count++;
-            }
+            AlignDots(TwentyFourHrDotRadius, 15, false);
         }
 
         //update when the datetime changes
         private void Timer_Tick_24(object sender, object e)
         {
             Second.Rotation = (DateTime.Now.Second * 6) - 90;
-            Minute.Rotation = (DateTime.Now.Minute * 6) -90;
+            Minute.Rotation = (DateTime.Now.Minute * 6) - 90;
             Hour.Rotation = (DateTime.Now.Hour * 15) + (DateTime.Now.Minute * 0.25) - 90;
         }
         private void Timer_Tick_12(object sender, object e)
         {
-            Second.Rotation = (DateTime.Now.Second * 6) -90;
-            Minute.Rotation = (DateTime.Now.Minute * 6) -90;
-            Hour.Rotation = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5) -90;
+            Second.Rotation = (DateTime.Now.Second * 6) - 90;
+            Minute.Rotation = (DateTime.Now.Minute * 6) - 90;
+            Hour.Rotation = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5) - 90;
         }
 
-        private void Change_Click(object sender, RoutedEventArgs e)
-        {
-            if (clockState == "twelve")
-            {
-                clockState = "twentyfour";
-                TwentyFourHrClock();
-            }
-            else
-            {
-                clockState = "twelve";
-                TwelveHrClock();
-            }
-        }
+       
 
         private void SetCanvasProperties(int[] propertyValues)
         {
@@ -192,7 +177,7 @@ namespace AnalogueClock
             }
         }
 
-        private void calculateNumberAlignment(int r, int initialangle,List<TextBlock> valueNumberBlocks,int incrementSkip,int incrementAngle)
+        private void calculateNumberAlignment(int r, int initialangle, List<TextBlock> valueNumberBlocks, int incrementSkip, int incrementAngle)
         {
             double x1, y1;
             int inccount = 0;
@@ -212,10 +197,88 @@ namespace AnalogueClock
             }
 
         }
+
+        //Align dots
+        private void AlignDots(int rdot, int inc, bool twelveHrFormat)
+        {
+            int count = 0;
+            foreach (var i in allDotsTextBlock)
+            {
+                double x1dot, y1dot;
+
+                if (count % 5 == 0 && twelveHrFormat)
+                {
+                    i.FontSize = 40;
+                    x1dot = (130 + rdot * Math.Cos(Math.PI * (count * inc) / 180.0) - 4);
+                    y1dot = (110 - rdot * Math.Sin(Math.PI * (count * inc) / 180.0) - 20);
+                }
+                else
+                {
+                    i.FontSize = 20;
+                    x1dot = (130 + rdot * Math.Cos(Math.PI * (count * inc) / 180.0));
+                    y1dot = (110 - rdot * Math.Sin(Math.PI * (count * inc) / 180.0));
+                }
+
+                i.SetValue(Canvas.LeftProperty, x1dot);
+                i.SetValue(Canvas.TopProperty, y1dot);
+                count++;
+            }
+        }
+        //reset hand size
+        private void ResetHandSize()
+        {
+            secondHand.X1 = 115; minuteHand.X1 = 105;hourHand.X1 = 70;
+        }
+
+        private void Decrement_Click(object sender, RoutedEventArgs e)
+        {
+            if (outerBlack.Width > 220)
+            {
+                DecreaseClockSize();
+                if (clockState == "twelve")
+                {
+                    AlignDots(TweleveHrDotRadius -= 10, 6, true);
+                }
+                else
+                {
+                    AlignDots(TwentyFourHrDotRadius -= 10, 15, false);
+                }
+            }
+        }
+
+        private void Increment_Click(object sender, RoutedEventArgs e)
+        {
+            if (outerBlack.Width < 440)
+            {
+                IncreaseClockSize();
+                if (clockState == "twelve")
+                {
+                    AlignDots(TweleveHrDotRadius += 10, 6, true);
+                }
+                else
+                {
+                    AlignDots(TwentyFourHrDotRadius += 10, 15, false);
+                }
+            }
+        }
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            if (clockState == "twelve")
+            {
+                clockState = "twentyfour";
+                this.TwentyFourHrDotRadius = 135;
+                ResetHandSize();
+                TwentyFourHrClock();
+
+            }
+            else
+            {
+                clockState = "twelve";
+                this.TweleveHrDotRadius = 120;
+                ResetHandSize();
+                TwelveHrClock();
+
+            }
+        }
     }
 }
-//main commit m1 testing for rebase,
-
-
-
-//updating main to m2
